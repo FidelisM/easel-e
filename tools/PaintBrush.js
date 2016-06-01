467eec243582f3648d9b31b220c4a9fba37429ac
nf071590 <nf071590@gmail.com>
2016-06-01 11:15:04 -0400

    forgot to save paintBrush

--- tools/PaintBrush.js
+++ tools/PaintBrush.js
@@ -40,16 +40,6 @@ PaintBrush.prototype.draw = function (ctx) {
   }
 };
 
-function printEvent(e) {
-  console.log('Button Info:\n\tbutton: ' + e.button +'\n\tbuttons: ' + e.buttons +
-    '\nClient:\n\tclientX: ' + e.clientX + '\n\tclientY: ' + e.clientY +
-    '\nLayer:\n\tlayerX: ' + e.layerX + '\n\tlayerY: ' + e.layerY +
-    '\nMovement:\n\tmovementX: ' + e.movementX + '\n\tmovementY: ' + e.movementY +
-    '\nOffset:\n\toffsetX: ' + e.offsetX + '\n\toffsetY: ' + e.offsetY +
-    '\nPage:\n\tpageX: ' + e.pageX + '\n\tpageY: ' + e.pageY +
-    '\nScreen:\n\tscreenX: ' +  e.screenX + '\n\tscreenY: ' + e.screenY);
-}
-
 /**
  * The different event functions for the PaintBrush
  * when it is bound and unbound these will be passed through a function
